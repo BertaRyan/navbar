678414fb3db54e45e05d8adc0589db8bed63a551
rBertamxs <ryanberta1234@gmail.com>
2025-06-27 13:39:24 -0700

    added styles

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,4 +1,4 @@
-import {useState} from 'react'
+import {useState, useRef} from 'react'
 import {links, social} from "./data"
 import { FaBarsStaggered } from "react-icons/fa6";
 import logo from './logo.svg'
